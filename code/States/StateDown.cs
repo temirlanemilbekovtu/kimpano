5199ad4377ad2b25a1e696dd05e8e672e5982fe9
Temirlan Emilbekov <emilbekov.tute@gmail.com>
2024-11-05 22:34:09 +0600

    feat(state): implement `StateDown` class behavior and transitions
    - Added `_animationName` export to define animation for the down state.
    - Implemented `HandleDamage` to transition to `StateDeath` on damage.
    - Added `StateEntry` and `StateExit` to manage animation playback and cleanup.
    - Created `OnAnimationFinished` callback to transition to `StateIdle` upon animation end.
    - Defined transitions to `StateIdle` and `StateDeath` via export properties.
    
    This completes the core functionality for `StateDown` in the character state machine.

--- code/States/StateDown.cs
+++ code/States/StateDown.cs
@@ -3,5 +3,28 @@ namespace Godot;
 [GlobalClass]
 public partial class StateDown : State
 {
+    [Export] private StringName _animationName;
     
+    [ExportGroup("Transitions")] 
+    [Export] private StateIdle _idle;
+    [Export] private StateDeath _death;
+
+    public override void HandleDamage() {
+        MyStateMachine.SwitchState(_death, new string[] { });
+    }
+    
+    public override void StateEntry(string[] args) {
+        MyAnimationPlayer.Play(_animationName);
+        MyAnimationPlayer.AnimationFinished -= OnAnimationFinished;
+        MyAnimationPlayer.AnimationFinished += OnAnimationFinished;
+    }
+
+    public override void StateExit() {
+        MyAnimationPlayer.AnimationFinished -= OnAnimationFinished;
+        MyAnimationPlayer.Stop();
+    }
+
+    private void OnAnimationFinished(StringName anim) {
+        MyStateMachine.SwitchState(_idle, new string[] { });
+    }
 }
