6d64c8b0aeade0f67bb9982f1f9903af982125bd
Temirlan Emilbekov <emilbekov.tute@gmail.com>
2024-11-13 09:08:21 +0600

    feat(states): implement `StateCooldown` class logic and transitions
    - Completed the `StateCooldown` class with essential transition and event handling logic.
    - Added transitions to `StateIdle` and `StateDeath` states:
      - `OnAnimEvent`: switches to `StateIdle` when animation completes.
      - `HandleDamage`: switches to `StateDeath` on taking damage.
    - Included setup and teardown for animation event listeners in `StateEntry` and `StateExit` methods.
    - Ensures proper handling of cooldown state transitions and character lifecycle.

--- code/States/StateCooldown.cs
+++ code/States/StateCooldown.cs
@@ -3,5 +3,24 @@ namespace Godot;
 [GlobalClass]
 public partial class StateCooldown : State
 {
-    
+	[ExportGroup("Transitions")]
+	[Export] private StateIdle  _idle;
+	[Export] private StateDeath _death;
+
+	public override void StateEntry(string[] args) {
+		MyAnimationPlayer.AnimationFinished -= OnAnimEvent;
+		MyAnimationPlayer.AnimationFinished += OnAnimEvent;
+	}
+
+	public override void HandleDamage() {
+		MyStateMachine.SwitchState(_death, new string[] { });
+	}
+
+	public override void StateExit() {
+		MyAnimationPlayer.AnimationFinished -= OnAnimEvent;
+	}
+
+	private void OnAnimEvent(StringName animName) {
+		MyStateMachine.SwitchState(_idle, new string[] { });
+	}
 }
