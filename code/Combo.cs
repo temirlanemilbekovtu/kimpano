a98382a29c2e0aa509128d35f34ffbe3d953f42d
Temirlan Emilbekov <emilbekov.tute@gmail.com>
2024-11-13 21:40:49 +0600

    refactor!(combo): replace single animation with three distinct animations
    - Split the single `AnimationName` into three separate properties:
      `SwingAnimName`, `AttackAnimName`, and `CooldownAnimName`.
    - Added support for distinct animations for different combo states.
    
    BREAKING CHANGE:
    - Code using the old `AnimationName` property will need to be updated to handle
      the new split animation properties.

--- code/Combo.cs
+++ code/Combo.cs
@@ -6,7 +6,9 @@ namespace Godot;
 public partial class Combo : Resource
 {
     [Export] public StringName      Name            { get; private set; } = "Combo";
-    [Export] public StringName      AnimationName   { get; private set; }
+    [Export] public StringName     SwingAnimName    { get; private set; }
+    [Export] public StringName     AttackAnimName   { get; private set; }
+    [Export] public StringName     CooldownAnimName { get; private set; }
     [Export] public Rect2           HitBox          { get; private set; }
     [Export] public Array<MoveKey>  MoveList        { get; private set; } = new() { MoveKey.None };
 }
