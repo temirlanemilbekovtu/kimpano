6361a84d4bbe25b1ede0996e4cf27a13dd2ea690
Temirlan Emilbekov <emilbekov.tute@gmail.com>
2024-11-14 17:43:18 +0600

    feat(state): implement StateContrAttack class
    - Added the `StateContrAttack` class implementation to handle
      after-parry counterattack logic.
    - Introduced customizable wait time and animation for counterattack.
    - Added state transitions for idle, death, and attack states.
    - Implemented timer-based transition to idle state after counterattack.
    - Provides a structured flow for handling combo and damage during
      the counterattack phase.

--- code/States/StateContrAttack.cs
+++ code/States/StateContrAttack.cs
@@ -3,5 +3,41 @@ namespace Godot;
 [GlobalClass]
 public partial class StateContrAttack : State
 {
-    
+    [Export] private float      _waitTime;
+    [Export] private StringName _animationName;
+
+    [ExportGroup("Transitions")]
+    [Export] private StateIdle   _idle;
+    [Export] private StateDeath  _death;
+    [Export] private StateAttack _attack;
+
+    private Timer _timer = new();
+
+    public override void _Ready() {
+	    _timer.Timeout += OnTimerTimeout;
+    }
+
+    public override void StateEntry(string[] args) {
+	    MyAnimationPlayer.Play(_animationName);
+
+	    _timer.WaitTime = _waitTime;
+	    _timer.Start();
+    }
+
+    public override void HandleCombo(Combo combo) {
+	    MyStateMachine.SwitchState(_attack, new[] { combo.Name.ToString() });
+    }
+
+    public override void HandleDamage() {
+	    MyStateMachine.SwitchState(_death, new string[] { });
+    }
+
+    public override void StateExit() {
+	    MyAnimationPlayer.Stop();
+	    _timer.Stop();
+    }
+
+    private void OnTimerTimeout() {
+	    MyStateMachine.SwitchState(_idle, new string[] { });
+    }
 }
